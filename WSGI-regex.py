a17c1d85cb53aa8e4318c94f779d86f94308d094
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2021-11-13 12:43:58 +0100

    The Sorrows of Young Werther...

--- WSGI-regex.py
+++ WSGI-regex.py
@@ -2,4 +2,6 @@
 https://docs.python.org/3/howto/regex.html
 https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s20.html
 http://bumblebeesoftware.com/
+
+Thinking about a non-trivial example...
 '''
