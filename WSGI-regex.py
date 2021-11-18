9212ef2caf5d3b1c9765f8bea2c1773d31da3e9a
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-11-18 09:44:34 +0100

    Reg.ex.cess...

--- WSGI-regex.py
+++ WSGI-regex.py
@@ -13,9 +13,11 @@ import re
 # https://www.englishclub.com/ref/esl/Power_of_7/7_Thats_2948.php
 p = re.compile(r'\b(?P<word>\w+\s*)(?P=word)+\b')
 s = p.search('"It is true for all that that that that that'
-             ' that that refers to is not the same that that that that refers to..."').group()
+             ' that that refers to is not the same that that that that refers to..."').group().split()
 print(s)
+
+that2this = lambda t: 'what this '
 ## https://www.cristinacabal.com/?p=482
-p = re.compile(r'\b(\w+(\s*|[.]))(\1)+\b')
-f = p.search('"I think that that that that that teacher gave us is correct...."').group().split()
-print(f)
+p = re.compile(r'\b(\w+(\s+))(\1)+\b')
+r = p.sub(that2this, '"I think that that that that that teacher gave us is correct..."')
+print(r)
