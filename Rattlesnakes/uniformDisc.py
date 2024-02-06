684e75fca32b593c5be1c129779eae2170f9bb80
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-02-06 21:45:26 +0100

    Square to circle (and back?)

--- Rattlesnakes/uniformDisc.py
+++ Rattlesnakes/uniformDisc.py
@@ -4,23 +4,23 @@
 
 from matplotlib.pyplot import gca, scatter, title, show
 from numpy import sqrt, sin, cos, pi as π 
-from numpy.random import uniform
+from numpy.random import uniform as U
 
-## Generate N random points in polar coordinates
 N = 1000
-r, θ = sqrt(uniform(0, 1, N)), uniform(0, 2*π, N)
+## Generate N random points in polar coordinates
+R, θ = sqrt(U(0, 1, N)), U(0, 2*π, N)
 
-#  Note that r is now not uniformly distributed. 
-#  In order to get a uniform distribution of points 
-#  in the disk one needs a map with a constant Jacobian
+# Displaying the motley points
+gca().set_aspect('equal'); title('X = √R⋅cosθ and Y = √R⋅sinθ')
+#  Note the square root of R makes the Jacobian of the map constant. 
+#  Indeed:
+#         ⌈ (1/2√R⋅cosθ  -√R⋅sinθ ⌉
+#      det|                       | = 2⁻¹(cos²θ + sin²θ),
+#         ⌊ (1/2√R⋅sinθ   √R⋅cosθ ⌋
 #
-#       ⌈ (1/(2√r))cosθ	    -√rsinθ ⌉
-#    det|                           | = (1/2)cos²θ+(1/2)sin²θ, 
-#       ⌊ (1/(2√r))sinθ	     √rcosθ ⌋
-#
-#  which is quite a famous identity... ;)
+#  is constant by virtue of a quite famous identity... ;)
+scatter(R * cos(θ), R * sin(θ), marker = '.', color = 'r'); show()
 
-# Visualize the points
-gca().set_aspect('equal')
-scatter(r * cos(θ), r * sin(θ), marker = '.', color = 'r')
-title('x = √r⋅cosθ and y = √r⋅sinθ'); show()
+# Usage (in PowerShell):
+# $pyth_on_path = "[...]"
+# &$pyth_on_path .\uniformDisc.py
